4440b29dc081f7db489c551b739ad9cf5b56f19f
Valentin Agachi <github-com@agachi.name>
2022-12-16 18:04:27 +0100

    Use the official VisWiz Github Action

--- tests/teardown.js
+++ tests/teardown.js
@@ -1,45 +1,7 @@
-const fs = require('fs');
-const VisWiz = require('viswiz-sdk');
-const puppeteer = require('puppeteer');
-const config = require('./config');
-
-function random(list) {
-	return list[Math.floor(Math.random() * list.length)];
-}
-
-function getCommitName() {
-	if (process.env.GITHUB_COMMIT_MESSAGE) {
-		return process.env.GITHUB_COMMIT_MESSAGE;
-	}
-	if (fs.existsSync('tmp/commits.txt')) {
-		const commits = fs
-			.readFileSync('tmp/commits.txt', 'utf-8')
-			.replace(/\n$/, '')
-			.split('\n');
-		return random(commits);
-	} else {
-		return 'Dev-' + new Date().toISOString();
-	}
-}
-
-module.exports = async function() {
+module.exports = async function () {
 	console.log('Stopping express...');
 	global.__SERVER__.close();
 
 	console.log('Stopping Puppeteer...');
 	await global.__BROWSER__.close();
-
-	if (process.env.VISWIZ_API_KEY && process.env.VISWIZ_PROJECT_ID) {
-		console.info('Sending build to VisWiz...');
-		const client = new VisWiz(process.env.VISWIZ_API_KEY, {
-			server: process.env.VISWIZ_SERVER,
-		});
-
-		await client.buildWithImages({
-			branch: process.env.GITHUB_HEAD_REF || process.env.GITHUB_BRANCH_NAME || 'master',
-			name: getCommitName(),
-			projectID: process.env.VISWIZ_PROJECT_ID,
-			revision: process.env.GITHUB_COMMIT_SHA || 'dev-' + new Date().toISOString(),
-		}, config.outputDir);
-	}
 };
